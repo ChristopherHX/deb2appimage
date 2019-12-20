2d385d7d09f314e06ae0f2c465acc0bf2b02c61a
Christopher Homberger <christopher.homberger@web.de>
2019-12-20 21:28:09 +0100

    arm test

--- deb2appimage.sh
+++ deb2appimage.sh
@@ -284,14 +284,14 @@ function buildappimage() {
     if [[ -z "$APP_VERSION" ]] || [[ "$APP_VERSION" = "null" ]]; then
         APP_VERSION="$(date +'%F')"
     fi
-    curl -sL "https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage" -o "$HOME"/.cache/deb2appimage/appimagetool || d2aexit 3 "appimagetool"
+    curl -sL "https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage" -o "$HOME"/.cache/deb2appimage/appimagetool.Appimage || d2aexit 3 "appimagetool"
+    chmod +x "$HOME"/.cache/deb2appimage/appimagetool.Appimage
+    "$HOME"/.cache/deb2appimage/appimagetool.Appimage --appimage-extract
     curl -sL "https://github.com/AppImage/AppImageKit/releases/download/12/runtime-armhf" -o "$HOME"/.cache/deb2appimage/AppRun-armhf
-    chmod +x "$HOME"/.cache/deb2appimage/appimagetool
-    bash
     if [[ "$D2A_QUIET" = "TRUE" ]]; then
-        ARCH="armhf" "$HOME"/.cache/deb2appimage/appimagetool --runtime-file "$HOME"/.cache/deb2appimage/AppRun-armhf "$@" "$HOME"/.cache/deb2appimage/AppDir "$D2A_OUTPUT"/"$APP_NAME"-"$APP_VERSION"-"armhf".AppImage > /dev/null 2>&1 || d2aexit 6 "$APP_NAME"
+        ARCH="armhf" squashfs-root/AppRun --runtime-file "$HOME"/.cache/deb2appimage/AppRun-armhf "$@" "$HOME"/.cache/deb2appimage/AppDir "$D2A_OUTPUT"/"$APP_NAME"-"$APP_VERSION"-"armhf".AppImage > /dev/null 2>&1 || d2aexit 6 "$APP_NAME"
     else
-        ARCH="armhf" "$HOME"/.cache/deb2appimage/appimagetool --runtime-file "$HOME"/.cache/deb2appimage/AppRun-armhf "$@" "$HOME"/.cache/deb2appimage/AppDir "$D2A_OUTPUT"/"$APP_NAME"-"$APP_VERSION"-"armhf".AppImage || d2aexit 6 "$APP_NAME"
+        ARCH="armhf" squashfs-root/AppRun --runtime-file "$HOME"/.cache/deb2appimage/AppRun-armhf "$@" "$HOME"/.cache/deb2appimage/AppDir "$D2A_OUTPUT"/"$APP_NAME"-"$APP_VERSION"-"armhf".AppImage || d2aexit 6 "$APP_NAME"
     fi
 }
 
